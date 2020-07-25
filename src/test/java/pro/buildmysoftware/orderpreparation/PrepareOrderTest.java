87eb37d1b1f0a9d9e8bef2e00d3561aadb5460e4
Kuba <kuba@mail.com>
2020-07-26 01:19:11 +0200

    DDD advanced training

--- src/test/java/pro/buildmysoftware/orderpreparation/PrepareOrderTest.java
+++ src/test/java/pro/buildmysoftware/orderpreparation/PrepareOrderTest.java
@@ -12,66 +12,63 @@ import static org.assertj.core.api.Assertions.catchThrowableOfType;
 
 class PrepareOrderTest {
 
-	// @formatter:off
-	@DisplayName("can prepare order")
-	//@formatter:on
-	@Test
-	void prepareOrder() throws Exception {
-		// when
-		Order order = orderWithMaxTotalCost(usd(100));
+    @DisplayName("can prepare order")
+    @Test
+    void prepareOrder() throws Exception {
+        // when
+        Order order = orderWithMaxTotalCost(usd(100));
 
-		// then
-		assertThat(order).isNotNull();
-	}
+        // then
+        assertThat(order).isNotNull();
+    }
 
-	// @formatter:off
-	@DisplayName("given max $100 order total cost, " +
-			"when add item of value $20 to the order, " +
-			"then item is successfully added to the order")
-	@Test
-	void addElementSuccessfully() throws Exception {
-		// given
-		var order = orderWithMaxTotalCost(usd(100));
-		var item = itemOfPrice(usd(20));
+    @DisplayName("given max $100 order total cost, " +
+            "when add item of value $20 to the order, " +
+            "then item is successfully added to the order")
+    @Test
+    void addElementSuccessfully() throws Exception {
+        // given
+        var order = orderWithMaxTotalCost(usd(100));
+        var item = itemOfPrice(usd(20));
 
-		// when
-		var event = order.add(item);
+        // when
+        var event = order.add(item);
 
-		// then
-		assertThat(event.getAllItems()).containsOnly(item);
-		assertThat(event.getItem()).isEqualTo(item);
-		assertThat(event.getOrder()).isEqualTo(order.id());
-		assertThat(event.getTotalCost()).isEqualTo(usd(20));
-		assertThat(order.totalCost()).isEqualTo(usd(20));
-	}
+        // then
+        assertThat(event.getAllItems()).containsOnly(item);
+        assertThat(event.getItem()).isEqualTo(item);
+        assertThat(event.getOrder()).isEqualTo(order.id());
+        assertThat(event.getTotalCost()).isEqualTo(usd(20));
+        assertThat(order.totalCost()).isEqualTo(usd(20));
+    }
 
-	@DisplayName("given order with max total cost $100, " +
-			"when add item of price $20 and then another item of price $81, " +
-			"then the last item cannot be added")
-	@Test
-	void addItemUnsuccessful() throws Exception {
-		// given
-		var order = orderWithMaxTotalCost(usd(100));
+    @DisplayName("given order with max total cost $100, " +
+            "when add item of price $20 and then another item of price $81, " +
+            "then the last item cannot be added")
+    @Test
+    void addItemUnsuccessful() throws Exception {
+        // given
+        var order = orderWithMaxTotalCost(usd(100));
 
-		// when
-		order.add(itemOfPrice(usd(20)));
-		var exceptionWhenAddingLastItem = catchThrowableOfType(() -> order.add(itemOfPrice(usd(81))),
-				MaxTotalCostExceededException.class);
+        // when
+        order.add(itemOfPrice(usd(20)));
+        var exceptionWhenAddingLastItem = catchThrowableOfType(() -> order.add(itemOfPrice(usd(81))),
+                MaxTotalCostExceededException.class);
 
-		// then
-		assertThat(exceptionWhenAddingLastItem).isNotNull()
-				.hasMessage("Max total cost exceeded: 101.0");
-	}
+        // then
+        assertThat(exceptionWhenAddingLastItem).isNotNull()
+                .hasMessage("Max total cost exceeded: 101.0");
+    }
 
-	private Order orderWithMaxTotalCost(Money usd) {
-		return Order.create(usd(100), order -> true);
-	}
+    private Order orderWithMaxTotalCost(Money usd) {
+        return Order.create(usd(100), order -> true);
+    }
 
-	private Item itemOfPrice(Money price) {
-		return TestFixtures.itemOfPrice(price);
-	}
+    private Item itemOfPrice(Money price) {
+        return TestFixtures.itemOfPrice(price);
+    }
 
-	private Money usd(double amount) {
-		return TestFixtures.usd(amount);
-	}
+    private Money usd(double amount) {
+        return TestFixtures.usd(amount);
+    }
 }
